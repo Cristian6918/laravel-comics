e4b726496047d0434b6049d97ec27b0ffa62f8b4
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-05-18 18:58:49 +0200

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+<div class="ct-header">
+    <div class="container">
+        <img src="assets/images/dc-logo.png" alt="">
+        <div class='ct-link'>
+            <a href="#">CHARATCTERS</a>
+        </div>
+        <div class='ct-link'>
+            <a href="#">COMICS</a>
+        </div>
+        <div class='ct-link'>
+            <a href="#">MOVIES</a>
+        </div>
+        <div class='ct-link'>
+            <a href="#">TV</a>
+        </div>
+        <div class='ct-link'>
+            <a href="#">GAMES</a>
+        </div>
+        <div class='ct-link'>
+            <a href="#">COLLECTIBLES</a>
+        </div>
+        <div class='ct-link'>
+            <a href="#">VIDEOS</a>
+        </div>
+        <div class='ct-link'>
+            <a href="#">FANS</a>
+        </div>
+        <div class='ct-link'>
+            <a href="#">NEWS</a>
+        </div>
+        <div class='ct-link'>
+            <a href="#">SHOP</a>
+        </div>
+
+
+    </div>
+</div>
