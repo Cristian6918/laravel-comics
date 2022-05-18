315f9d8a4299977f1618c0d12fe923ba7abd1657
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-05-18 20:03:09 +0200

    footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,68 @@
+<div class="main-section">
+    <div class="container">
+        <div class="left-side">
+            <div class="list-part">
+                <ul>DC Comics <li><a href="#characters">Characters </a></li>
+                    <li><a href="#comics">Comics </a></li>
+                    <li><a href="#movies">Movies </a></li>
+                    <li><a href="#tv">Tv </a></li>
+                    <li><a href="#games">Games </a></li>
+                    <li><a href="#videos">Videos </a></li>
+                    <li><a href="#news">News </a></li>
+                </ul>
+            </div>
+            <div class="list-part">
+                <ul>Shop <li><a href="#shop-dc">Shop Dc </a>
+                    </li>
+                    <li><a href="#shop-dc-collectables">Shop DC Collectables </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="list-part">
+                <ul>dc <li><a href="#terms-of-use">Terms of Use
+                        </a></li>
+                    <li><a href="#privacy-policy">Privacy Policy (New) </a></li>
+                    <li><a href="#ad-choices">Ad Choices </a></li>
+                    <li><a href="#advertising">Advertising </a></li>
+                    <li><a href="#jobs">Jobs </a></li>
+                    <li><a href="#subscription">Subscription </a></li>
+                    <li><a href="#talent-workshops">Talent Workshops </a></li>
+                    <li><a href="#cpsc-certificates">CPSC Certificates </a></li>
+                    <li><a href="#ratings">Ratings </a></li>
+                    <li><a href="#shop-help">Shop Help </a></li>
+                    <li><a href="#contact-us">Contact Us </a></li>
+                </ul>
+            </div>
+            <div class="list-part">
+                <ul>Sites <li><a href="#dc">DC </a></li>
+                    <li><a href="#mad-magazine">MAD Magazine </a></li>
+                    <li><a href="#dc-kids">DC Kids </a></li>
+                    <li><a href="#dc-universe">DC Universe </a></li>
+                    <li><a href="#dc-power-visa">DC Power Visa </a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="right-side"></div>
+    </div>
+</div>
+<div class="footer">
+    <div class="container-footer">
+        <button class="sign-up-button">SIGN-UP NOW!</button>
+        <a href="#" class="follow">FOLLOW US</a>
+        <a>
+            <img src="assets/images/footer-facebook.png" alt="facebook">
+        </a>
+        <a>
+            <img src="assets/images/footer-twitter.png" alt="twitter">
+        </a>
+        <a>
+            <img src="assets/images/footer-youtube.png" alt="youtube">
+        </a>
+        <a>
+            <img src="assets/images/footer-pinterest.png" alt="pinterest">
+        </a>
+        <a>
+            <img src="assets/images/footer-periscope.png" alt="periscope">
+        </a>
+    </div>
+</div>
